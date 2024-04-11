2472e66b2231a573d1dfd0352623f001962a8ab8
Jada Brown <jbrowbb@siue.edu>
2024-04-11 13:32:37 -0500

    double checking for work

--- Project1/scraper.py
+++
@@ -1,7 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from spiders.spider import ArticleSpider
-
-def scrape_articles():
-    process = CrawlerProcess()
-    process.crawl(ArticleSpider)
-    process.start()
