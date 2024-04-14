cf4eae659cd029dfdb313ca2b00f93950a351515
Jada Brown <jbrowbb@siue.edu>
2024-04-14 10:36:30 -0500

    updating module_1.py

--- CS325_p2/module_1/module_1.py
+++ CS325_p2/module_1/module_1.py
@@ -1,20 +1,17 @@
-import os
+from pathlib import Path
 
-def get_urls(articles_file):
-    """ Reads URLs form a text file
+def get_urls(article_file: Path):
+    """ Reads URLs form a text file.
 
-        Args:
-            article_file: Path to the file containing URLs (assumed to be in "Data/raw").
+    Args:
+        articles_file: Path to the file containing URLs
 
-        Returns:
-            A list of URLs    
+    Returns:
+        A list of URLs to be scraped
     """
 
-    # Costruct abolte path based on expected filder structure
-    articles_file = os.path.join('Data', 'raw', articles_file)
-
     # Reads URLs from the file
-    with open(articles_file, 'r') as file:
+    with article_file.open('r') as file:
         urls = [line.strip() for line in file.readlines()]
 
     return urls
