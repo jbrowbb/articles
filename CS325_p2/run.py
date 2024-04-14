83dde886294579726bfb9538e41052a1e40fc82c
Jada Brown <jbrowbb@siue.edu>
2024-04-14 09:49:57 -0500

    fixing spellings

--- CS325_p2/run.py
+++ CS325_p2/run.py
@@ -2,7 +2,7 @@ from module_1 import get_urls
 from module_2 import scrape_arctiles
 
 if __name__=="__main__":
-    """ SCrapes articles from text file and saces them to separate files."""
+    """ Scrapes articles from text file and saces them to separate files."""
 
     # replace with path if not correct (assumption it is in "Data/raw")
     articles_file = "articles.txt"
