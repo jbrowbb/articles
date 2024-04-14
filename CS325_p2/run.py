1b9761e688aa18e600462aa3822b0b5503fc6a7b
Jada Brown <jbrowbb@siue.edu>
2024-04-14 11:49:50 -0500

    fixing paths

--- CS325_p2/run.py
+++ CS325_p2/run.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     """ Scrapes articles from text file and saves them to separate files. """
 
     # Path to article file with article urls, can change
-    articles_file = Path(__file__).resolve().'Data'/'raw'/'articles.txt'
+    articles_file = Path(__file__).resolve().parent /'Data'/'raw'/'articles.txt'
 
     # Get Urls from text file
     urls = get_urls(articles_file)
